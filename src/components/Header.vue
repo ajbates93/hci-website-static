d128296f1cb3257ae28e9d80134512ec79df8f00
Alex Bates <ajbates93@gmail.com>
2023-02-02 21:51:41 +0000

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const active = ref(false)
+const toggleNavigation = () => active.value = !active.value
+</script>
+
+<template>
+  <nav class="fixed top-0 w-full left-0 bg-gray-800 text-white">
+    <button @click="toggleNavigation">Click Me</button>
+    {{active ? 'Hello' : 'Bye'}}
+  </nav>
+</template>
